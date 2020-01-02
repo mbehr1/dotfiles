209a0d8f6ee42874afd3f55fdd6e4219b7d62f54
Matthias Behr <mbehr@mcbehr.de>
2020-01-02 22:03:38 +0100

    First version.
    A first set with oh-my-zsh and powerlevel10k configs.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # dotfiles
 My zsh,... config files and an install script
+
+## install
+
+run ./setup.sh
+
+This installs (if not installed yet):
+ - oh-my-zsh
+ - powerlevel10k
+
+## todo
+
+Test plugins:
+ - magic-enter
+ - tmux
---
+++ setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# install oh-my-zsh to default folder if not there yet:
+if [ -d ~/.oh-my-zsh ]; then
+    echo "oh-my-sh exists already. skipping install"
+else 
+    echo Install oh-my-zsh to default folder:
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+# install powerlevel10k plugin/theme if not there yet:
+if [ -d ~/.oh-my-zsh/custom/themes/powerlevel10k ]; then
+    echo "powerlevel10k already installed. skipping install"
+else
+    echo Install powerlevel10k plugin/theme
+    git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+fi
+
+# create symlinks to new config files:
+
+if [ -L ~/.zshrc ]; then
+    echo ".zshrc is already a symlink. skipping install"
+else
+    # backup .zshrc
+    echo "creating backup of current .zshrc"
+    mv -iv ~/.zshrc ~/.zshrc.org_dotfiles_setup
+
+    echo "creating symlink to .zshrc"
+    ln -snv "$PWD/.zshrc" ~/.zshrc
+fi
+
+if [ -L ~/.p10k.zsh ]; then
+    echo ".p10k.zsh is already a symlink. skipping install"
+else
+    # backup
+    echo "creating backup of current .p10k.zsh"
+    mv -iv ~/.p10k.zsh ~/.p10k.zsh.org_dotfiles_setup
+
+    echo "creating symlink to .p10k.zsh"
+    ln -snv "$PWD/.p10k.zsh" ~/.p10k.zsh
+fi
+
+echo "done :-)"
+
