f07ea69fc62230663dd37d932d016266a47d54bd
Matthias Behr <mbehr@mcbehr.de>
2020-01-04 09:44:14 +0100

    Another try to stop zsh to run during install.

--- setup.sh
+++ setup.sh
@@ -15,8 +15,7 @@ if [ -d ~/.oh-my-zsh ]; then
     echo "oh-my-sh exists already. skipping install"
 else 
     echo Install oh-my-zsh to default folder:
-    RUNZSH=no
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
 # install powerlevel10k plugin/theme if not there yet:
